ff73b07e281389f1b36886c2b9c4e21874325d3f
dkang339 <dkang339@gatech.edu>
2026-05-07 20:06:21 -0400

    tighten site description meta text

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
 	title: `Dayoung Kang`,
-	description: `Personal website of Dayoung Kang, PhD student in Computational Science and Engineering at Georgia Tech.`,
+	description: `Dayoung Kang, PhD student in Computational Science and Engineering at Georgia Tech`,
 	url: `https://dkang339.github.io`,
 	name: `Dayoung Kang`,
 	enableThemeSwitcher: false,
